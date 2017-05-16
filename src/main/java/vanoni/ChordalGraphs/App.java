d9a8a3fcce0eecfabb5f675affc133f012f95eae
Gabriele Vanoni <gabriele.vanoni@mail.polimi.it>
2017-05-16 23:47:08 +0200

    First real commit

---
+++ src/main/java/vanoni/ChordalGraphs/App.java
@@ -0,0 +1,13 @@
+package vanoni.ChordalGraphs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
