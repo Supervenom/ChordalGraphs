1c5767dc85f4776ad82901bdf86747f03f7ef4cb
Gabriele Vanoni <gabriele.vanoni@mail.polimi.it>
2017-05-20 00:04:24 +0200

    Implemented MCS algorithm

---
+++ src/main/java/vanoni/ChordalGraphs/ChordalAgorithms.java
@@ -0,0 +1,56 @@
+package vanoni.ChordalGraphs;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import edu.uci.ics.jung.graph.UndirectedGraph;
+
+public class ChordalAgorithms {
+
+	@SuppressWarnings("unused")
+	private void ChordalAlgorithms() {
+	}
+	
+	public static void maximumCardinalitySearch(UndirectedGraph<Vertex,Integer> graph) {
+		
+		List<List<Vertex>> set = new LinkedList<>();
+		List<Vertex> order = new LinkedList<>();
+		
+		int i,j = 0;
+		
+		int n = graph.getVertexCount();
+		
+		for (i = 0; i < n; i++)
+			set.add(i, new LinkedList<Vertex>());
+		
+		for (Vertex v: graph.getVertices()) {
+			v.setSize(0);
+			set.get(0).add(v);
+		}
+		
+		for (i = 0; i < n; i++) { //Ordering starts from 0
+			Vertex v = set.get(j).remove(0); // any, I chose to remove the first one
+			v.setPosition(i);
+			order.add(i, v);
+			v.setSize(-1);
+			
+			for (Integer e: graph.getIncidentEdges(v)) {
+				Vertex u = graph.getEndpoints(e).getSecond().equals(v) 
+						   ? graph.getEndpoints(e).getFirst() : graph.getEndpoints(e).getSecond();
+				if (u.getSize() > -1) {
+					set.get(u.getSize()).remove(u);
+					u.setSize(u.getSize()+1);
+					set.get(u.getSize()).add(u);
+				}
+			}
+			
+			j++;
+			
+			while (j >= 0 && set.get(j).isEmpty())
+				j--;
+			
+		}
+		
+	}
+	
+}
---
+++ src/main/java/vanoni/ChordalGraphs/Vertex.java
@@ -0,0 +1,36 @@
+package vanoni.ChordalGraphs;
+
+public class Vertex {
+
+	private int alpha;
+	private int size;
+
+	/**
+	 * @return the position
+	 */
+	public int getAlpha() {
+		return alpha;
+	}
+
+	/**
+	 * @param position the position to set
+	 */
+	public void setPosition(int alpha) {
+		this.alpha = alpha;
+	}
+
+	/**
+	 * @return the size
+	 */
+	public int getSize() {
+		return size;
+	}
+
+	/**
+	 * @param size the size to set
+	 */
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+}
